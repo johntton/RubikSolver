ccd6b1ddd7df7eb824681911d84ea5ad577bd581
John Ton <johntton3@gmail.com>
2025-11-20 16:34:23 -0800

    Modified img var to extern

--- src/CubeScanner.cpp
+++ src/CubeScanner.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 // Global Variables
-cv::Mat img;
+extern cv::Mat img;
 int gridSize = 300;
 int cellSize = gridSize / 3;
 
@@ -58,7 +58,7 @@ class CubeScanner {
                 int roiHeight = cellSize;
 
                 // Takes ROI and stores in array
-                cv::Rect roi(roiX, roiY, cellSize, cellSize);
+                cv::Rect roi(roiX, roiY, roiWidth, roiHeight);
                 roiArray[row][col] = frame(roi).clone();
             }
         }
@@ -81,54 +81,3 @@ class CubeScanner {
         return allFaces;
     }
 };
-
-int main() {
-    cv::VideoCapture cap(1);
-    CubeScanner Scanner;
-    std::array<std::array<cv::Mat, 3>, 3> roiArray;
-
-    while (true) {
-        cap.read(img);
-        if (img.empty()) break;
-
-        int w = img.cols;
-        int h = img.rows;
-        int side = std::min(w, h); // Find the smaller dimension
-
-        // Center crop
-        int x = (w - side) / 2;
-        int y = (h - side) / 2;
-
-        cv::Rect roi(x, y, side, side);
-        cv::Mat square = img(roi);
-
-        Scanner.drawGrid(square);
-        auto faceROI = Scanner.extractROIs(square);
-        cv::imshow("Video", square);
-
-        // Test that each cell is correct
-        // cv::imshow("0, 0", roiArray[0][0]);
-        // cv::imshow("0, 1", roiArray[0][1]);
-        // cv::imshow("0, 2", roiArray[0][2]);
-
-        // cv::imshow("1, 0", roiArray[1][0]);
-        // cv::imshow("1, 1", roiArray[1][1]);
-        // cv::imshow("1, 2", roiArray[1][2]);
-
-        // cv::imshow("2, 0", roiArray[2][0]);
-        // cv::imshow("2, 1", roiArray[2][1]);
-        // cv::imshow("2, 2", roiArray[2][2]);
-
-        char key = cv::waitKey(1);
-
-        if (key == ' ') {
-            Scanner.saveFace(faceROI);
-        }
-
-        if (Scanner.finished()) {
-            break;
-        }
-
-        if (key == 'q') return 0;
-    }
-}
